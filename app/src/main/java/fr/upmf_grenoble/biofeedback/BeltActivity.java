baa26f72acdf805db791b7cf5d5a3168b73afdf8
LopesRafael <rafalopes1212@gmail.com>
2015-05-26 17:21:38 +0200

    Ajout des différentes activités

---
+++ app/src/main/java/fr/upmf_grenoble/biofeedback/BeltActivity.java
@@ -0,0 +1,15 @@
+package fr.upmf_grenoble.biofeedback;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class BeltActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belt);
+    }
+}
--- app/src/main/java/fr/upmf_grenoble/biofeedback/MainActivity.java
+++ app/src/main/java/fr/upmf_grenoble/biofeedback/MainActivity.java
@@ -1,7 +1,9 @@
 package fr.upmf_grenoble.biofeedback;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -10,5 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openBiofeedbackActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, BiofeedbackActivity.class);
+        startActivity(intent);
+    }
+
+    public void openBeltActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, BeltActivity.class);
+        startActivity(intent);
     }
 }
